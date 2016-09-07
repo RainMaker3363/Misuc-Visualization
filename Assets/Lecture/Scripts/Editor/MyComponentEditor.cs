f5565aa9b75b7a1b7f0ab2bcb0617dc94f0d7c20
rainmaker3363 <jsc22637@gmail.com>
2016-09-07 10:51:26 +0900

    Music_Visualization #1
    1일차 Music_Visualization

---
+++ Assets/Lecture/Scripts/Editor/MyComponentEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MyComponent))]
+public class MyComponentEditor : Editor
+{
+    // 유니티 캐쉬(cach)에서 값을 받아오는 역할이다.
+    SerializedProperty intvariable;
+    SerializedProperty floatvariable;
+    SerializedProperty gameObjects;
+
+    void OnEnable()
+    {
+        Debug.Log("Click");
+
+        intvariable = serializedObject.FindProperty("intVariable");
+        floatvariable = serializedObject.FindProperty("floatVariable");
+        gameObjects = serializedObject.FindProperty("gameObjects");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(intvariable, new GUIContent("var1"));
+        EditorGUILayout.PropertyField(floatvariable, new GUIContent("var2"));
+
+        EditorGUILayout.PropertyField(gameObjects, new GUIContent("List"), true);
+
+        // 자동 관리
+        serializedObject.ApplyModifiedProperties();
+
+
+        // 수동 관리
+        MyComponent myComponent = (MyComponent)target;
+
+        //myComponent.intVariable = EditorGUILayout.IntField("int variable", myComponent.intVariable);
+        //myComponent.floatVariable = EditorGUILayout.Slider("float variable", myComponent.floatVariable, 0f, 100f);
+        myComponent.intvar = EditorGUILayout.IntField("int var", myComponent.intvar);
+
+        if(GUILayout.Button("DoSomething"))
+        {
+            myComponent.DoSomething();
+        }
+    }
+
+}
---
+++ Assets/Lecture/Scripts/MyComponent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+
+    public int intVariable;
+    public float floatVariable;
+    public GameObject[] gameObjects;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int intvar
+    {
+        get
+        {
+            return intVariable;
+        }
+        set
+        {
+            intVariable = value;
+        }
+    }
+
+    public void DoSomething()
+    {
+        intVariable += 1;
+    }
+}
