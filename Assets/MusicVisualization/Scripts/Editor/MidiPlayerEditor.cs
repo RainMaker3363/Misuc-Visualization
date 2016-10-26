c2c2759f288782972c38e9ac9f5aa843fb407079
rainmaker3363 <jsc22637@gmail.com>
2016-10-26 12:42:28 +0900

    Week 8
    - MidiPlayer 완성
    - MidiPlayerEditor 완성
    - MidiPlayer의 음악 파일 및 Midi파일 동기화 조정 완료

---
+++ Assets/MusicVisualization/Scripts/Editor/MidiPlayerEditor.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+//[CustomEditor(typeof(MidiPlayer))]
+public class MidiPlayerEditor : Editor {
+
+    SerializedProperty midi;
+    SerializedProperty Music;
+    SerializedProperty audioSource;
+    SerializedProperty playDelayTime;
+    SerializedProperty playSpeed;
+
+
+    void OnEnable()
+    {
+        midi = serializedObject.FindProperty("midi");
+        midi = serializedObject.FindProperty("Music");
+        midi = serializedObject.FindProperty("audioSource");
+        midi = serializedObject.FindProperty("playDelayTime");
+        midi = serializedObject.FindProperty("playSpeed");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(midi);
+        EditorGUILayout.PropertyField(Music);
+        EditorGUILayout.PropertyField(audioSource);
+        EditorGUILayout.PropertyField(playDelayTime);
+        EditorGUILayout.PropertyField(playSpeed);
+
+        serializedObject.ApplyModifiedProperties();
+
+        MidiPlayer midiPlayer = (MidiPlayer)target;
+
+        // 현재 어플레케이션이 실행중인지 체크한다.
+        if(Application.isPlaying == true)
+        {
+            GUILayout.BeginHorizontal();
+
+            if(midiPlayer.isPlaying == true)
+            {
+                if (GUILayout.Button("Pause") == true)
+                {
+                    midiPlayer.Pause();
+                }
+
+                // 에디터에게 다시 랜더링을 시키라는 의미이다.
+                // 대신 많이 호출하면 연산량이 많아지므로 주의
+                EditorUtility.SetDirty(target);
+            }
+            else
+            {
+                if(midiPlayer.playTime == 0)
+                {
+                    if (GUILayout.Button("Play") == true)
+                    {
+                        midiPlayer.Play();
+                    }
+                }
+                else
+                {
+
+                    if (GUILayout.Button("Resume") == true)
+                    {
+                        midiPlayer.Resume();
+                    }
+                }
+            }
+
+            if (GUILayout.Button("Stop") == true)
+            {
+                midiPlayer.Stop();
+            }
+
+            GUILayout.EndHorizontal();
+
+            // 재생시간 Bar를 그려주는 역할이다.
+            GUILayout.HorizontalSlider(midiPlayer.playTime, 0f, midiPlayer.totalTime);
+            EditorGUILayout.LabelField(string.Format("Time : {0:F1} sec", midiPlayer.playTime));
+        }
+    }
+}
---
+++ Assets/MusicVisualization/Scripts/MidiPlayer.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Eidtor 모드에서도 아래의 함수들이 작동되게 만든다.
+//[ExecuteInEditMode]
+public class MidiPlayer : MonoBehaviour {
+
+    public MidiAsset midi;
+
+    public AudioClip Music;
+    public AudioSource audioSource;
+
+    public float playDelayTime = 0.0f;
+    public float playSpeed = 1.0f;
+
+    private bool _isPlaying = false;
+    private float _playTime = 0.0f;
+    private float _totalTime = 0.0f;
+    private float _audioDelayTime;
+    private float _midiDelayTime;
+
+    private bool _audioStarted = false;
+
+    private MidiTrack[] _tracks;
+    private int[] _noteIndex;
+    private float _pulseTime;
+
+	// Use this for initialization
+	void Start () {
+
+        audioSource.clip = Music;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(_isPlaying == true)
+        {
+            if(_audioDelayTime > 0f)
+            {
+                _audioDelayTime -= Time.deltaTime;
+            }
+            else
+            {
+                if(_audioStarted == false)
+                {
+                    _audioStarted = true;
+
+                    if (audioSource != null)
+                    {
+                        audioSource.Play();
+                    }
+
+                }
+
+            }
+
+
+            if (_midiDelayTime > 0f)
+            {
+                _midiDelayTime -= Time.deltaTime;
+            }
+            else
+            {
+                _playTime += Time.deltaTime;
+
+                for (int i = 0; i < _tracks.Length; i++)
+                {
+                    for (int j = _noteIndex[i]; j < _tracks[i].Number; j++)
+                    {
+                        MidiNote note = _tracks[i].Notes[j];
+                        float sTime = note.StartTime * _pulseTime;
+                        float eTime = note.EndTime * _pulseTime;
+
+                        if (_playTime < sTime)
+                            break;
+
+                        if (_playTime > eTime)
+                            _noteIndex[i] = j + 1;
+                    }
+                }
+            }
+            //_playTime += Time.deltaTime;
+        }
+	}
+
+    public void Play()
+    {
+        if(midi == null)
+        {
+            return;
+        }
+
+        if (Music != null && audioSource != null)
+        {
+            audioSource.clip = Music;
+        }
+
+        if(audioSource != null)
+        {
+            audioSource.Play();
+        }
+
+        _isPlaying = true;
+        _playTime = 0.0f;
+        _totalTime = midi.TotalTime;
+        _pulseTime = midi.PulseTime;
+
+        if(playDelayTime == 0f)
+        {
+            _audioDelayTime = 0.0f;
+            _midiDelayTime = 0f;
+        }
+        else if(playDelayTime > 0.0f)
+        {
+            _audioDelayTime = playDelayTime;
+            _midiDelayTime = 0f;
+        }
+        else
+        {
+            _audioDelayTime = 0f;
+            _midiDelayTime = -playDelayTime;
+        }
+
+        _audioStarted = false;
+
+        _tracks = midi.Tracks;
+        _noteIndex = new int[_tracks.Length];
+
+        for (int i = 0; i < _noteIndex.Length; i++)
+            _noteIndex[i] = 0;
+    }
+
+    public void Pause()
+    {
+        _isPlaying = false;
+
+        if (audioSource != null)
+        {
+            audioSource.Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        _isPlaying = true;
+
+        if (audioSource != null)
+        {
+            audioSource.UnPause();
+        }
+    }
+
+    public void Stop()
+    {
+        _isPlaying = false;
+        _playTime = 0.0f;
+
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    public bool isPlaying
+    {
+        get
+        {
+            return _isPlaying;
+        }
+    }
+
+    public float totalTime
+    {
+        get
+        {
+            return _totalTime;
+        }
+    }
+
+    public float playTime
+    {
+        get
+        {
+            return _playTime;
+        }
+    }
+}
