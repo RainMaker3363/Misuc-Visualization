d5a0a36c2c0321f5c377c7b126303ca76e36c448
RainMaker3363 <jsc22637@gmail.com>
2016-12-13 22:52:55 +0900

    Week 15
    - 완성

--- Assets/MusicVisualization/Scripts/SolarVortex/MainVortex.cs
+++ Assets/MusicVisualization/Scripts/SolarVortex/MainVortex.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class MainVortex : MidiEventTrigger
@@ -7,6 +8,10 @@ public class MainVortex : MidiEventTrigger
     //private ParticleSystem RippleParticle;
 
     private bool RotationOn;
+
+    public UnityEvent eventPlay;
+    public UnityEvent eventPause;
+    public UnityEvent eventStop;
 
     // Use this for initialization
     void Start()
@@ -29,21 +34,26 @@ public class MainVortex : MidiEventTrigger
     protected override void OnPlay()
     {
         RotationOn = true;
+
+        eventPlay.Invoke();
     }
 
     protected override void OnPause()
     {
         RotationOn = false;
+        eventPause.Invoke();
     }
 
     protected override void OnResume()
     {
         RotationOn = true;
+        eventPlay.Invoke();
     }
 
     protected override void OnStop()
     {
         RotationOn = false;
+        eventStop.Invoke();
     }
 
     protected override void OnNoteOn()
